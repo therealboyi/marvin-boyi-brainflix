772f1c7fb64539066e2741c137f248e46041edbf
BOYI <user@BOYIs-MacBook-Air.local>
2024-06-06 20:35:31 -0400

    Created NavBar component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+import Nav from " ../Nav/Nav";
+const Header = () => {
+  return (
+    <header>
+      {/* <Link to="/"></Link>Kombucha Music Shop
+      <Nav />
+      Shop <Nav /> */}
+    </header>
+  );
+};
+export default Header;
